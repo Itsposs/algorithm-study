0e2f6555f6a69a9ea896887ca77546fb4f1b7386
hvate huo <Itsposs@163.com>
2019-09-27 19:38:58 +0800

    add bubble.cc

---
+++ bubblesort/bubble1.cc
@@ -0,0 +1,78 @@
+
+// c++11
+// const int MAXSIZE = 20;
+// constexpr int MAXSIZE = 20;
+#define MAXSIZE 20
+
+// using KeyType = int;
+typedef int KeyType;
+typedef int InfoType;
+
+typedef struct {
+	KeyType key;
+	InfoType otherinfo;
+} RedType;
+
+typedef struct {
+	RedType r[MAXSIZE + 1];
+	int length;
+} SqList;
+
+#include <random> // random
+// debug
+//#include <iostream>
+void CreateSqList(SqList &L) {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(1, 100);
+
+	for (int i = 1; i <= MAXSIZE; ++i) {
+		int key = dis(gen);
+		//std::cout << key << " ";
+		L.r[i].key = key;
+		L.length++;
+	}
+	//std::cout << std::endl;
+}
+
+void BubbleSort(SqList &L) {
+
+	int flag = 1;
+	int m = L.length - 1;
+	while ((m > 0)&&(flag == 1)) {
+		
+		flag = 0;
+		for (int j = 1; j <= m; ++j) {
+			if(L.r[j].key > L.r[j + 1].key) {
+				flag = 1;
+				std::swap(L.r[j], L.r[j + 1]);
+			}
+		}
+		--m;
+	}
+}
+
+#include <iostream>
+
+void Print(SqList L) {
+	
+	for (int i = 1; i <= L.length; ++i) {
+		std::cout << L.r[i].key << " ";
+	}
+	std::cout << std::endl;
+}
+
+void test() {
+	SqList L;
+	CreateSqList(L);
+	Print(L);
+	BubbleSort(L);
+	Print(L);
+}
+
+int main(int argc, char *argv[]) {
+
+	test();
+
+	return 0;
+}
